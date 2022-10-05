c45f6276922f07b80be82c581169f8a5c244954a
mavor <ma_voroshilov@mail.ru>
2022-10-05 15:34:58 +0300

    Add function, that rounds killed ship with missed shots

--- GameClasses/HideBoard.cs
+++ GameClasses/HideBoard.cs
@@ -21,6 +21,37 @@ public class HideBoard
         this.linkedBoard = linkedBoard;
     }
 
+    private bool IsValidTile(int x, int y)
+    {
+        return (x >= 0 && x < 10 && y >= 0 && y < 10);
+    }
+
+    public void RoundMissedShots(int x, int y)
+    {
+        int[] movesX = { 1, -1, 0, 0 };
+        int[] movesY = { 0, 0, 1, -1 };
+        for (int i = 0; i < 4; ++i)
+        {
+            int currentX = x;
+            int currentY = y;
+            while (IsValidTile(currentX, currentY) &&
+                   linkedBoard.board[currentX, currentY])
+            {
+                for (int j = 0; j < 4; ++j)
+                {
+                    int neighbourX = currentX + movesX[j];
+                    int neighbourY = currentY + movesY[j];
+                    if (IsValidTile(neighbourX, neighbourY) && board[neighbourX, neighbourY] != TileCondition.Hit)
+                    {
+                        board[neighbourX, neighbourY] = TileCondition.Missed;
+                    }
+                }
+            }
+            currentX += movesX[i];
+            currentY += movesY[i];
+        }
+    }
+
     public ShootResult OpenTile(int x, int y)
     {
         if (linkedBoard.board[x, y])
@@ -36,6 +67,7 @@ public class HideBoard
 
         if (IsShipKilled(x, y))
         {
+            RoundMissedShots(x, y);
             return ShootResult.Killed;
         }
 
@@ -56,7 +88,7 @@ public class HideBoard
             {
                 coords[j, 0] += moves[j].Item1;
                 coords[j, 1] += moves[j].Item2;
-                if (!isMissed[j] || coords[j, 0] < 0 || coords[j, 1] < 0 || coords[j, 0] >= 10 || coords[j, 1] >= 10)
+                if (!isMissed[j] || !IsValidTile(coords[j, 0], coords[j, 1]))
                     continue;
                 if (linkedBoard.board[coords[j, 0], coords[j, 1]] &&
                     board[coords[j, 0], coords[j, 1]] == TileCondition.Unknown)
